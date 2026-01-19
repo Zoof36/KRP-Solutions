7dbb2a46db79cc34059aa6929178aac964d2a11b
Zoof36 <135020149+Zoof36@users.noreply.github.com>
2026-01-19 22:25:47 +0200

    updated css and js files

--- contact.js
+++ contact.js
@@ -33,12 +33,9 @@
   });
 
 }());
-
-<script>
   const toggle = document.querySelector('.menu-toggle');
   const nav = document.querySelector('.nav');
 
   toggle.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
-</script>
